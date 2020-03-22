08af085077e91e072723f7bd9a53cc3b7d7e9b19
nam <cnam0303@gmail.com>
2020-03-23 00:17:34 +0700

    upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,23 @@ import "../style.css"
 
 
 export default class Home extends Component {
-    render() {
+    componentDidMount() {
+        setInterval(() => {
+            var i = Math.floor((Math.random() * 10) + 1);
+            var source = '/' + i.toString() + '.jpg'
+            var image = document.getElementById('image')
+            image.src = source
+        }, 8000);
+    }
+
+     render() {
         return (
             <div className="center">
                 <h1 id="bienvenue">BIENVENUE À MON PAGE</h1>
                 <h2 id="name">- OWEN CHẤN NAM NGUYỄN - </h2>
                 <h4 id="title">Un voyageur - a food lover</h4>
                 <br/>
-                <img src='/0.jpg' />
+                <img src='/17.jpg' id="image"/>
             </div>
         )
     }
