b871946007dfdb99570e517de44dabcc86747d34
nam <cnam0303@gmail.com>
2020-03-22 17:46:29 +0700

    upload

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../asset')));
 app.use(express.static(path.join(__dirname, '../uploads')));
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'))
 });
 
